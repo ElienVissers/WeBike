28534fa400db504a9e19a45fd931e36265e2102d
Elien Vissers <elien.visserssimilon@gmail.com>
2019-02-17 18:19:29 +0100

    add ProfileLogo and integrate it in navigation header

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { View, Text, Button, Image, TextInput } from "react-native";
+import { View, Text, Button, Image, TextInput, TouchableWithoutFeedback } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 import {FadeInImage} from './fadeinimage';
+import {ProfileLogo} from './profilelogo';
+
+let isNewUser = true;
 
 class WelcomeScreen extends React.Component {
     constructor(props) {
@@ -12,9 +15,19 @@ class WelcomeScreen extends React.Component {
     }
     static navigationOptions = ({navigation}) => {
         return {
-            title: navigation.getParam('title', 'Welcome!')
+            title: navigation.getParam('title', 'Welcome!'),
+            headerRight: (
+                <TouchableWithoutFeedback
+                    onPress={navigation.getParam('editProfile')}
+                >
+                    <ProfileLogo/>
+                </TouchableWithoutFeedback>
+            )
         };
     };
+    componentDidMount() {
+        this.props.navigation.setParams({ editProfile: this.onPress });
+    }
     onPress() {
         if (this.state.typedName) {
             this.props.navigation.navigate('ProfileRoute', {
@@ -26,26 +39,28 @@ class WelcomeScreen extends React.Component {
     }
     render() {
         return (
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "pink" }}>
-                <FadeInImage source={require('./assets/bike.jpg')} style={{width: 150, height: 100}}/>
-                <Text>Welcome Screen</Text>
-                <Text>welcome message and current weather info</Text>
-                <TextInput
-                    style={{height: 40, color: 'white'}}
-                    placeholder="Type your name!"
-                    onChangeText={text => this.setState({
-                        text: text,
-                        typedName: true
-                    })}
-                />
-                <Button
-                    title="Create profile"
-                    onPress={this.onPress}
-                />
-                <Button
-                    title="Update the title"
-                    onPress={() => this.props.navigation.setParams({title: 'Updated!'})}
-                />
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "white" }}>
+                <FadeInImage source={require('./assets/bike.png')} style={{flex:1, height: 150, width: 150}} resizeMode="contain" />
+                <View style={{flex: 2}}>
+                    <Text>Welcome Screen</Text>
+                    <Text>welcome message and current weather info blabla</Text>
+                    <TextInput
+                        style={{height: 40, color: 'black'}}
+                        placeholder="Type your name!"
+                        onChangeText={text => this.setState({
+                            text: text,
+                            typedName: true
+                        })}
+                    />
+                    <Button
+                        title="Create profile"
+                        onPress={this.onPress}
+                    />
+                    <Button
+                        title="Update the title"
+                        onPress={() => this.props.navigation.setParams({title: 'Updated!'})}
+                    />
+                </View>
             </View>
         );
     }
@@ -57,30 +72,49 @@ class ProfileScreen extends React.Component {
     }
     static navigationOptions = ({ navigation }) => {
         return {
-          title: 'Edit your profile, ' + navigation.getParam('name', 'cyclist')
+            title: 'Edit your profile, ' + navigation.getParam('name', 'cyclist')
         };
     };
     render() {
         const name = this.props.navigation.getParam('name', 'cyclist');
         return (
-          <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "lightblue" }}>
-            <FadeInImage source={require('./assets/bike.jpg')} style={{width: 150, height: 100}}/>
-            <Text>Profile Screen</Text>
-            <Text>Hi {name}, set up your profile here! :)</Text>
-            <Text>input name, city and weekly cycling schedule</Text>
-            <Text>type of bike? weekly goal in km?</Text>
-            <Button
-                title="Go to profile... again"
-                onPress={() => this.props.navigation.push('ProfileRoute')}
-            />
-            <Button
-                title="Go to Home"
-                onPress={() => this.props.navigation.navigate('WelcomeRoute')}
-            />
-            <Button
-                title="Go back"
-                onPress={() => this.props.navigation.goBack()}
-            />
+          <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "white" }}>
+            <FadeInImage source={require('./assets/bike.png')} style={{flex:1, height: 150, width: 150}} resizeMode="contain" />
+            <View style={{flex: 2}}>
+                <Text>Profile Screen</Text>
+                <Text>Hi {name}, set up your profile here! :)</Text>
+                <Text>input name, city and weekly cycling schedule</Text>
+                <Text>type of bike? weekly goal in km?</Text>
+                <Button
+                    title="Go to profile... again"
+                    onPress={() => this.props.navigation.push('ProfileRoute')}
+                />
+                <Button
+                    title="Go to Home"
+                    onPress={() => this.props.navigation.navigate('WelcomeRoute')}
+                />
+            </View>
+          </View>
+        );
+    }
+}
+
+class CurrentWeatherScreen extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    static navigationOptions = ({ navigation }) => {
+        return {
+            title: 'Current weather for Berlin'
+        };
+    };
+    render() {
+        return (
+          <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "white" }}>
+            <FadeInImage source={require('./assets/bike.png')} style={{flex:1, height: 150, width: 150}} resizeMode="contain" />
+            <View style={{flex: 2}}>
+                <Text>Profile Screen</Text>
+            </View>
           </View>
         );
     }
@@ -90,25 +124,19 @@ class ProfileScreen extends React.Component {
 const AppNavigator = createStackNavigator(
     {
         WelcomeRoute: WelcomeScreen,
-        ProfileRoute: ProfileScreen
+        ProfileRoute: ProfileScreen,
+        CurrentWeatherRoute: CurrentWeatherScreen
     },
     {
-        initialRouteName: "WelcomeRoute",
+        initialRouteName: isNewUser ? "WelcomeRoute" : "CurrentWeatherRoute",
         defaultNavigationOptions: {
             headerStyle: {
-                backgroundColor: 'palevioletred'
+                backgroundColor: 'skyblue'
             },
             headerTintColor: 'white',
             headerTitleStyle: {
                 fontWeight: 'bold'
-            },
-            headerRight: (
-                <Button
-                    onPress={() => alert('This is a button!')}
-                    title="Info"
-                    color="black"
-                />
-            )
+            }
         }
     }
 );
---
+++ profilelogo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Image} from 'react-native';
+
+export class ProfileLogo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+        <Image
+            {...this.props}
+            source={require('./assets/bike.png')}
+            style={{flex:1, width: 50, height: 35, margin: 10}}
+            resizeMode="contain"
+        />
+        );
+    }
+
+}
